a1dce3903df493986a81e87be96a20170060398a
Vasim Bukhari <vbukhari86@gmail.com>
2014-02-24 11:30:35 -0500

    further improvement - working project 1

--- Project1/Project1.py
+++ Project1/Project1.py
@@ -1,5 +1,6 @@
 #	@author: Vasim Bukhari
 #	Analysis of Alforithms: Project 1 
+#
 import sys
 import os
 
@@ -10,48 +11,51 @@ def main():
 		nTeams = 0		#Number of Teams
 		nGames = 0		#Number of Games played
 		#Opening and reading input.txt file 
-		with open('input2.txt', 'r') as inputFile:
-			nDataSet = int(inputFile.readline())		#first line state number of data sets
-			print "Number of DataSets: %d" % nDataSet
-			#iterate over to collect all data sets from input file
-			for i in range(nDataSet):				
-				inputFile.readline()					#Skip second line of input file
-				nTeamsNGames = inputFile.readline().split(' ')	
-				nTeams = int(nTeamsNGames[0])			#Number of Teams
-				nGames = int(nTeamsNGames[1]) 			#Number of Games played
-				print "Number of teams: %d" % nTeams
-				print "Number of games: %d" % nGames
-				teamsDict ={}							#creating and initializing Dictionary
-				#obj = Fixtures(0, "")
-				#iterate over to collect name of teams from input file
-				for j in range(nTeams):
-					#teamsName.append(inputFile.readline().rstrip().split(' '))
-					line = inputFile.readline().strip()
-					obj = Fixtures(j, line[0])
-					teamsDict[line] = obj#.__dict__
-					#dict((key, value) for key, value in team1.__dict__.iteritems())
-					#teamsDict[team1.teamName]= team1
-					#teamsDict['name'].append(inputFile.readline().rstrip().split(' '))
-				#print teamsDict
-				#iterate over to collect all games results from input file
-				for k in range(nGames):
-					#scoreTable.append(inputFile.readline().strip().split(' '))
-					line = inputFile.readline().strip().split()  
-					homeT = line[0]			#Name of home team
-					homeG = int(line[1])	#Number of goals of home team
-					awayG = int(line[3])	#Number of goals of away team
-					awayT = line[4]			#Name of away team
-					#print homeT, homeG, awayT, awayG
-					teamsDict[homeT].updateTeam(homeG, awayG)
-					teamsDict[awayT].updateTeam(awayG, homeG)
-					#obj.__dict__.update(teamsDict)
+		with open('input.txt', 'r') as inputFile:
+			with open('output3.txt', 'w') as outFile:
+				nDataSet = int(inputFile.readline())	#first line state number of data sets
+				print "Number of DataSets: %d" % nDataSet
+				#iterate over to collect all data sets from input file
+				for i in range(nDataSet):				
+					inputFile.readline()					#Skip second line of input file
+					nTeamsNGames = inputFile.readline().split(' ')	
+					nTeams = int(nTeamsNGames[0])			#Number of Teams
+					nGames = int(nTeamsNGames[1]) 			#Number of Games played
+					# print "Number of teams: %d" % nTeams
+					# print "Number of games: %d" % nGames
+					teamsDict ={}							#creating and initializing Dictionary
 
-				sortedFixture = sorted(teamsDict, key=cmp_to_key(num_compare))
-				#sortedFixture = sorted(teamsDict.values(), cmp=Fixtures.cmp_to_key)
-				# for key, value in teamsDict.iteritems():
-				# 	#for x in value:
-				# 	print value
-				
+					#iterate over to collect name of teams from input file
+					for j in range(nTeams):
+						#teamsName.append(inputFile.readline().rstrip().split(' '))
+						line = inputFile.readline().strip()
+						obj = Fixtures(j, line)
+						teamsDict[line] = obj#.__dict__
+
+					#iterate over to collect all games results from input file
+					for k in range(nGames):
+
+						line = inputFile.readline().strip().split()  
+						homeT = line[0]			#Name of home team
+						homeG = int(line[1])	#Number of goals of home team
+						awayG = int(line[3])	#Number of goals of away team
+						awayT = line[4]			#Name of away team
+						#print homeT, homeG, awayT, awayG
+						teamsDict[homeT].updateFixture(homeG, awayG)
+						teamsDict[awayT].updateFixture(awayG, homeG)
+
+					# print teamsDict
+					sortedFixture = sorted(teamsDict.values(), cmp=Fixtures.number_compare)
+					#sortedFixture = sorted(teamsDict, key=Fixtures.number_compare)
+					num=0
+					outFile.write("%d\n"% nTeams)
+					outFile.write("%d. %s\n" % (num+1, sortedFixture[0]))
+					for x in range(1, nTeams):
+						if not (sortedFixture[x-1].checkRanking(sortedFixture[x])):
+							num = x
+						outFile.write("%d. %s\n"% (x+1, sortedFixture[x]))
+					outFile.write('\n')
+
 	except IOError as e:
 		print "I/O error({0}): {1}".format(e.errno, e.strerror)
 	except ValueError:
@@ -71,62 +75,74 @@ class Fixtures(object):
 		self.numGoals = 0
 		self.numSuffGoals = 0
 		self.goalDifference = 0
-		self.percentPoints = 0
-		# for k, v in dictionary.items():
-		# 	setattr(self, k, v)
+		self.percentPoints = 0.00
 
 	#Method updates the number of games played, number of goals(Earn, Suffered), 
 	#and number of points, num
-	def updateTeam(self, homeG, awayG):
+	def updateFixture(self, homeG, awayG):
 		#if win game, add 3 points 
 		if (homeG > awayG):	self.numPoints += 3
 		#if draw, add 1 points
 		elif(homeG == awayG): self.numPoints += 1 
 		#if lost, no point added
 		else: pass
+		#update number of games played
+		self.numGames += 1
 		#update number of goals earned
 		self.numGoals = self.numGoals + homeG		
 		#update number of goals suffered	
 		self.numSuffGoals = self.numSuffGoals + awayG	
 		#update number of goals difference
-		self.goalDifference = self.numGoals - self.numSuffGoals	
+		self.goalDifference = self.findGoalDiff()
 		#update number of percentPoints
-		if(self.numGames == 0):	self.percentPoints = 100
-		else: self.percentPoints = 100*(self.numPoints/(3*self.numGames))
+		self.percentPoints = self.findPercentPoint()
+	
 
-		#self.__dict__.update(dictionary)
-
-	def num_compare(x, y):
-		return x - y
-
-	def cmp_to_key(mycmp):
-
-		class K(object):
-			"""docstring for K"""
-			def __init__(self, obj, *args):
-				self.obj = obj
-			def __lt__(self, other):
-				return mycmp(self.obj, other.obj) < 0
-	        def __gt__(self, other):
-	            return mycmp(self.obj, other.obj) > 0
-	        def __eq__(self, other):
-	            return mycmp(self.obj, other.obj) == 0
-	        def __le__(self, other):
-	            return mycmp(self.obj, other.obj) <= 0
-	        def __ge__(self, other):
-	            return mycmp(self.obj, other.obj) >= 0
-	        def __ne__(self, other):
-	        	return mycmp(self.obj, other.obj) != 0
-
-	    return K
-				
+	def number_compare(x, y):
+		
+		if(x.numPoints > y.numPoints):	
+			return -1
+		elif(x.numPoints < y.numPoints):
+			return 1
+		elif(x.goalDifference > y.goalDifference): 
+			return -1
+		elif(x.goalDifference < y.goalDifference):
+			return 1
+		elif(x.numGoals > y.numGoals):	
+			return -1
+		elif(x.numGoals < y.numGoals):	
+			return 1
+		elif(x.teamPosition > y.teamPosition):
+			return -1
+		elif(x.teamPosition < y.teamPosition):
+			return 1
+		else:
+			return 0
+	def checkRanking(x, y):
+		if(x.numPoints == y.numPoints) and (x.goalDifference == y.goalDifference) and (x.numGoals == y.numGoals):
+			return True
+	def __str__(self):
+		return " ".join(map(str,
+		[self.teamName, self.numPoints, self.numGames,
+		self.numGoals, self.numSuffGoals, self.goalDifference,
+		"%.2f" % self.percentPoints]
+		))
+	#calculates the percent points
+	def findPercentPoint(self):
+		temp = 100.00
+		#self.percentPoints = 100 if no game is played
+		if(self.numGames == 0):	
+			return temp 		
+		else: 
+			#number of points divide with number of games played times 3 point
+			temp = 100.00*self.numPoints/(3*self.numGames)
+			return temp
 
 	#calculate goal difference
-	def goalDifference(goalScored, goalSuffered):
-		return abs(goalScored - goalSuffered)
-	#print team data
-	def printFixture():
-		pass
+	def findGoalDiff(self):
+	#def goalDifference(self):
+		return (self.numGoals - self.numSuffGoals)
+
 
 if __name__ == '__main__':
 	main()
